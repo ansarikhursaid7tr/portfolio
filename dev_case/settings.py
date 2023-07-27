7236819038d6c9c6602f7ca976a5c2d36335d6d5
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2023-07-27 16:06:27 +0545

    cleanup

--- dev_case/settings.py
+++ dev_case/settings.py
@@ -137,8 +137,12 @@ if DATABASE_URL:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "HOST": "portfolio.postgres.database.azure.com",
+            "USER": "nightowl",
+            "PORT": "5432",
+            "NAME": "postgres",
+            "PASSWORD": "A1stard1dmi7@"
         }
     }
 
